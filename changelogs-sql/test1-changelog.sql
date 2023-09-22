895cf7df1184e9623d3c5dba067e57060c78d432
Damion Thomas <damionthomas34@gmail.com>
2023-09-22 11:24:41 -0400

    added new changesets to changelogs-sql/test1-changelog.sql

--- changelogs-sql/test1-changelog.sql
+++ changelogs-sql/test1-changelog.sql
@@ -6,3 +6,7 @@ create table test1 (
     name varchar(255)
 );
 --rollback drop table test1;
+
+--changeset dthomas:2
+insert into test1 (id, name) values (1, 'Damion');
+insert into test1 (id, name) values (2, 'Damaris');
